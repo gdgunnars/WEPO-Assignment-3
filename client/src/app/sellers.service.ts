0d460d071ed321160fbf0afca106ec5c934e0204
gdgunnars <gdg@gdg.is>
2017-03-09 19:42:38 +0000

    added big R in the include statement for rjxs to work with linux

--- client/src/app/sellers.service.ts
+++ client/src/app/sellers.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable'
-import 'rxjs/rx';
+import 'rxjs/Rx';
 
 import { Seller } from './interfaces/seller';
 import { SellerProduct } from './interfaces/sellerproduct';
