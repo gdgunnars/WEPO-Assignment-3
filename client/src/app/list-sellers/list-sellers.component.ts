0cc79b060780f37481819b0cc0e5deacd2b6c1ad
gdgunnars <gdg@gdg.is>
2017-03-10 13:18:33 +0000

    fetched sellers on load and put them in a table

--- client/src/app/list-sellers/list-sellers.component.ts
+++ client/src/app/list-sellers/list-sellers.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { SellersService } from '../sellers.service';
+import { Seller } from '../interfaces/seller';
+import { SellerProduct } from '../interfaces/sellerproduct';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { SellerDialogComponent } from '../seller-dialog/seller-dialog.component';
 
 @Component({
 	selector: 'app-list-sellers',
 	templateUrl: './list-sellers.component.html',
 	styleUrls: ['./list-sellers.component.css']
 })
-export class ListSellersComponent implements OnInit {
 
-	constructor() { }
+export class ListSellersComponent implements OnInit {
+	private sellers: Seller[];
+
+	constructor(private service : SellersService, private modalService : NgbModal) {}
 
 	ngOnInit() {
+		this.service.getSellers().subscribe( result => {
+			this.sellers = result;
+		}, err => {
+			console.log('I was not able to get Sellers')
+		});
 	}
 
 }
