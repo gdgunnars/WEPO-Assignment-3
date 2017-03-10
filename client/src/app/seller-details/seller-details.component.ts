6dce1e623de61b2d39498a18471423bc674ee34e
Gudjon <gudjonss12@ru.is>
2017-03-10 17:40:23 +0000

    image in seller details now visible

--- client/src/app/seller-details/seller-details.component.ts
+++ client/src/app/seller-details/seller-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import {SellersService} from '../sellers.service';
+import {Seller} from '../interfaces/seller';
 
 @Component({
 	selector: 'app-seller-details',
@@ -9,12 +11,19 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class SellerDetailsComponent implements OnInit {
 
 	sellerId: number;
+	seller: Seller;
 
-	constructor(private router: Router,
+	constructor(private router: Router, private service: SellersService,
 				private route: ActivatedRoute) { }
 
 	ngOnInit() {
 		this.sellerId = +this.route.snapshot.params['id'];
+		this.service.getSellerById(this.sellerId).subscribe( result => {
+			console.log(result);
+			this.seller = result;
+		}, err => {
+			console.log("Was unable to retrieve seller information");
+			}
+		);
 	}
-
 }
