ee4267e528432b2c8f0f4b9a9b3613d50b408c76
gdgunnars <gdg@gdg.is>
2017-03-11 16:56:43 +0000

    Added layout for list-sellers and list details, which includes product details

--- client/src/app/seller-details/seller-details.component.ts
+++ client/src/app/seller-details/seller-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import {SellersService} from '../sellers.service';
-import {Seller} from '../interfaces/seller';
+import { SellersService} from '../sellers.service';
+import { Seller } from '../interfaces/seller';
+import { SellerProduct } from '../interfaces/sellerproduct';
 
 @Component({
 	selector: 'app-seller-details',
@@ -12,6 +13,8 @@ export class SellerDetailsComponent implements OnInit {
 
 	sellerId: number;
 	seller: Seller;
+	products: SellerProduct[];
+	top10: SellerProduct[];
 
 	constructor(private router: Router, private service: SellersService,
 				private route: ActivatedRoute) { }
@@ -19,11 +22,32 @@ export class SellerDetailsComponent implements OnInit {
 	ngOnInit() {
 		this.sellerId = +this.route.snapshot.params['id'];
 		this.service.getSellerById(this.sellerId).subscribe( result => {
-			console.log(result);
 			this.seller = result;
 		}, err => {
 			console.log("Was unable to retrieve seller information");
 			}
 		);
+
+		this.service.getSellerProducts(this.sellerId).subscribe( result => {
+			this.products = result;
+			this.sortProducts(this.products.slice());
+
+		}, err => {
+			console.log("Was not able to get Products for seller id: " + this.sellerId);
+		});
+
+	}
+
+	sortProducts(products) {
+		this.top10 = products.sort((n1, n2) => {
+			if(n1.quantitySold > n2.quantitySold) {
+				return -1;
+			}
+			if ( n1.quantitySold < n2.quantitySold) {
+				return 1;
+			}
+			return 0;
+		});
+		this.top10 = this.top10.slice(0, 10);
 	}
 }
