941ca61a8f1c7a6c5b1d6df8e860e01b6151f713
Gudjon <gudjonss12@ru.is>
2017-03-12 12:27:51 +0000

    100p code cov nav-bar, added router-stubs testing class

--- client/src/app/nav-bar/nav-bar.component.spec.ts
+++ client/src/app/nav-bar/nav-bar.component.spec.ts
@@ -1,14 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavBarComponent } from './nav-bar.component';
+import {NavBarComponent} from './nav-bar.component';
+import {Router} from "@angular/router";
+import { RouterLinkStubDirective } from '../router-stubs';
+import {RouterOutletStubComponent} from '../router-stubs';
+import {DebugElement, NO_ERRORS_SCHEMA} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe('NavBarComponent', () => {
 	let component: NavBarComponent;
 	let fixture: ComponentFixture<NavBarComponent>;
 
+
+	const mockRouter = {
+		navigate: jasmine.createSpy('navigate')
+	};
+
+
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ NavBarComponent ]
+			declarations: [ 
+				NavBarComponent,
+				RouterLinkStubDirective, 
+				RouterOutletStubComponent 
+				],
+				schemas: [ NO_ERRORS_SCHEMA ], 
+			providers: [{
+				provide: Router,
+				useValue: mockRouter
+			}]
 		})
 		.compileComponents();
 	}));
@@ -19,7 +39,7 @@ describe('NavBarComponent', () => {
 		fixture.detectChanges();
 	});
 
-	xit('should create', () => {
-		expect(component).toBeTruthy();
+	it('should create', () => {
+		expect(component).not.toBeNull();
 	});
 });
