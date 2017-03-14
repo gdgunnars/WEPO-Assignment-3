de20f784bc064f32e2656915dd4fc57770f2a22c
Gudjon <gudjonss12@ru.is>
2017-03-14 10:50:40 +0000

    Refactor tests in sellersservice

--- client/src/app/sellers.service.spec.ts
+++ client/src/app/sellers.service.spec.ts
@@ -8,6 +8,51 @@ import { Seller } from './interfaces/seller';
 import { SellerProduct } from './interfaces/sellerproduct';
 
 describe('SellersService', () => {
+	const sellersArray = [{
+							id: 1,
+							name: 'Gux',
+							category: 'Knitting',
+							imagePath: 'http://www.example.com'
+						}, {
+							id: 2,
+							name: 'Lommi',
+							category: 'Knitting',
+							imagePath: 'http://www.example.com'
+						}];
+
+	const seller = 	{
+						id: 2,
+						name: 'Lommi',
+						category: 'Knitting',
+						imagePath: 'http://www.example.com'
+					};
+
+	const productsArray = [{
+							id: 1,
+							name: 'Leirtau',
+							price: 1000,
+							quantitySold: 500,
+							quantityInStock: 5000,
+							imagePath: 'http://www.example.com'
+						}, {
+							id: 2,
+							name: 'Pottar',
+							price: 500,
+							quantitySold: 100,
+							quantityInStock: 1000,
+							imagePath: 'http://www.example2.com'
+						}];
+
+	const product = {
+						id: 1,
+						name: 'Leirtau',
+						price: 1000,
+						quantitySold: 500,
+						quantityInStock: 5000,
+						imagePath: 'http://www.example.com'
+					};
+
+
 	beforeEach(() => {
 		TestBed.configureTestingModule({
 			providers: [
@@ -21,102 +66,75 @@ describe('SellersService', () => {
 						return new Http(backendInstance, defaultOptions);
 					}
 				}
-				]
+			]
 		});
 	});
+
+	let subject: SellersService = null;
+	let backend: MockBackend = null;
+	beforeEach(inject([SellersService, MockBackend], (sellersService: SellersService, mockBackend: MockBackend) => {
+		subject = sellersService;
+		backend = mockBackend;
+	}));
+
 
 	it('should ...', inject([SellersService], (service: SellersService) => {
 		expect(service).toBeTruthy();
 	}));
 
-	describe('SellersServiceTests', () => {
-		let subject: SellersService = null;
-		let backend: MockBackend = null;
-
-		beforeEach(inject([SellersService, MockBackend], (sellersService: SellersService, mockBackend: MockBackend) => {
-			subject = sellersService;
-			backend = mockBackend;
-		}));
-
+	describe('SellersService get functions', () => {
 		it('getSellers should return list of sellers', (done) => {
+			// Arrange
 			backend.connections.subscribe((connection: MockConnection) => {
 				const options = new ResponseOptions({
-					body: JSON.stringify(
-						[{
-							id: 1,
-							name: 'Gux',
-							category: 'Knitting',
-							imagePath: 'http://www.example.com'
-						}, {
-							id: 2,
-							name: 'Lommi',
-							category: 'Knitting',
-							imagePath: 'http://www.example.com'
-						} 
-						])
+					body: JSON.stringify(sellersArray)
 				});
 				connection.mockRespond(new Response(options));
 			});
 
+			// Act
 			subject.
 			getSellers()
 			.subscribe((response) => {
 				expect(response.length).toBe(2);
 				expect(response[0].id).toBe(1);
-				expect(response[0].category).toEqual('Knitting')
+				expect(response[0].category).toEqual('Knitting');
 				expect(response[0].name).toEqual('Gux');
 				expect(response[1].name).toEqual('Lommi');
 				done();
 			});
 		});
-
+	});
 		it('getSellerById should return the seller matching the ID', (done) => {
+			// Arrange
 			backend.connections.subscribe((connection: MockConnection) => {
 				const options = new ResponseOptions({
-					body: JSON.stringify(
-						{
-							id: 2,
-							name: 'Lommi',
-							category: 'Knitting',
-							imagePath: 'http://www.example.com'
-						})
+					body: JSON.stringify(seller)
 				});
 				connection.mockRespond(new Response(options));
 			});
 
+			// Act
 			subject.
 			getSellerById(2)
 			.subscribe((response) => {
 				expect(response.id).toBe(2);
-				expect(response.category).toEqual('Knitting')
+				expect(response.category).toEqual('Knitting');
 				expect(response.name).toEqual('Lommi');
 				done();
 			});
 		});
 
 		it('getSellerProducts should return the products matching the ID of the seller', (done) => {
+			// Arrange
 			backend.connections.subscribe((connection: MockConnection) => {
 				const options = new ResponseOptions({
-					body: JSON.stringify(
-						[{
-							id: 1,
-							name: 'Leirtau',
-							price: 1000,
-							quantitySold: 500,
-							quantityInStock: 5000,
-							imagePath: 'http://www.example.com'
-						},{
-							id: 2,
-							name: 'Pottar',
-							price: 500,
-							quantitySold: 100,
-							quantityInStock: 1000,
-							imagePath: 'http://www.example2.com'
-						}])
+					body: JSON.stringify(productsArray)
 				});
 				connection.mockRespond(new Response(options));
 			});
 
+			// Act
 			subject.
 			getSellerProducts(2)
 			.subscribe((response) => {
@@ -127,60 +145,40 @@ describe('SellersService', () => {
 			});
 		});
 
+
+	describe('SellersService add functions', () => {
 		it('addSeller should return the seller matching the seller sent in as the function parameter', (done) => {
+			// Arrange
 			backend.connections.subscribe((connection: MockConnection) => {
 				const options = new ResponseOptions({
-					body: JSON.stringify(
-						{
-							id: 3,
-							name: 'Lodmundur',
-							category: 'Coffee',
-							imagePath: 'http://www.example.com'
-						})
+					body: JSON.stringify(seller)
 				});
 				connection.mockRespond(new Response(options));
 			});
 
+			// Act
 			subject.
-			addSeller(<Seller> 	{
-							id: 3,
-							name: 'Lodmundur',
-							category: 'Coffee',
-							imagePath: 'http://www.example.com'
-						})
+			addSeller(<Seller> seller)
 			.subscribe((response) => {
-				expect(response.id).toBe(3);
-				expect(response.name).toEqual('Lodmundur');
-				expect(response.category).toEqual('Coffee');
+				expect(response.id).toBe(2);
+				expect(response.name).toEqual('Lommi');
+				expect(response.category).toEqual('Knitting');
 				done();
 			});
 		});
 
 		it('addProduct should return the product matching the product sent in as the function parameter', (done) => {
+			// Arrange
 			backend.connections.subscribe((connection: MockConnection) => {
 				const options = new ResponseOptions({
-					body: JSON.stringify(
-						{
-							id: 1,
-							name: 'Leirtau',
-							price: 1000,
-							quantitySold: 500,
-							quantityInStock: 5000,
-							imagePath: 'http://www.example.com'
-						})
+					body: JSON.stringify(product)
 				});
 				connection.mockRespond(new Response(options));
 			});
 
+			// Act
 			subject.
-			addProduct(1, <SellerProduct>{
-							id: 1,
-							name: 'Leirtau',
-							price: 1000,
-							quantitySold: 500,
-							quantityInStock: 5000,
-							imagePath: 'http://www.example.com'
-						})
+			addProduct(1, <SellerProduct> product)
 			.subscribe((response) => {
 				expect(response.id).toBe(1);
 				expect(response.name).toEqual('Leirtau');
@@ -190,70 +188,50 @@ describe('SellersService', () => {
 				done();
 			});
 		});
+	});
 
+	describe('SellersService edit functions', () => {
 		it('editSeller should return the seller object matching the seller object sent in as the function parameter', (done) => {
+			// Arrange
 			backend.connections.subscribe((connection: MockConnection) => {
 				const options = new ResponseOptions({
-					body: JSON.stringify(
-					{
-						id: 3,
-						name: 'Gulli',
-						category: 'Hannyrdir',
-						imagePath: 'http://www.example.com'
-					})
+					body: JSON.stringify(seller)
 				});
 				connection.mockRespond(new Response(options));
 			});
 
+			// Act
 			subject.
-			editSeller(<Seller> {
-						id: 3,
-						name: 'Gulli',
-						category: 'Hannyrdir',
-						imagePath: 'http://www.example.com'
-					})
+			editSeller(<Seller> seller)
 			.subscribe((response) => {
-				expect(response.id).toBe(3);
-				expect(response.name).toEqual('Gulli');
-				expect(response.category).toEqual('Hannyrdir');
+				expect(response.id).toBe(2);
+				expect(response.name).toEqual('Lommi');
+				expect(response.category).toEqual('Knitting');
 				done();
 			});
 		});
 
 		it('editProduct should return the product object matching the product object sent in as the function parameter', (done) => {
+			// Arrange
 			backend.connections.subscribe((connection: MockConnection) => {
 				const options = new ResponseOptions({
-					body: JSON.stringify(
-					{
-						id: 3,
-						name: 'Pans',
-						price: 1000,
-						quantitySold: 5,
-						quantityInStock: 50,
-						imagePath: 'http://www.example.com'
-					})
+					body: JSON.stringify(product)
 				});
 				connection.mockRespond(new Response(options));
 			});
 
-			subject.
-			editProduct(1, <SellerProduct>
-			{
-				id: 3,
-				name: 'Pans',
-				price: 1000,
-				quantitySold: 5,
-				quantityInStock: 50,
-				imagePath: 'http://www.example.com'
-			})
+			// Act
+			subject
+			.editProduct(1, product)
 			.subscribe((response) => {
-				expect(response.id).toBe(3);
-				expect(response.name).toEqual('Pans');
+				expect(response.id).toBe(1);
+				expect(response.name).toEqual('Leirtau');
 				expect(response.price).toBe(1000);
-				expect(response.quantitySold).toBe(5);
-				expect(response.quantityInStock).toBe(50);
+				expect(response.quantitySold).toBe(500);
+				expect(response.quantityInStock).toBe(5000);
 				done();
 			});
 		});
 	});
+
 });
